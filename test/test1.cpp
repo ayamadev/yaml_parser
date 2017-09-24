b7c23ecf7070572d46862db8c84efcb3d9c00253
ayamaAtB <ayama9tlr215@gmail.com>
2017-09-24 17:51:45 +0900

    Add test cases

--- test/test1.cpp
+++ test/test1.cpp
@@ -1,30 +1,78 @@
-#include <yaml_parser.hpp>
+#define BOOST_TEST_MAIN
+#include <boost/test/included/unit_test.hpp>
+
+#include <iostream>
+#include <sstream>
 
 #include <boost/property_tree/json_parser.hpp>
 #include <boost/property_tree/xml_parser.hpp>
 
-int main(int argc, char** argv)
+#include <yaml_parser.hpp>
+
+using namespace boost::property_tree;
+using namespace yaml_parser;
+
+const char* str_yaml =
+    "a: b\n"
+    "c: d\n"
+    "e:\n"
+    "  f: g\n"
+    "  h: i\n"
+    "  j:\n"
+    "    - k\n"
+    "    - l"
+    ;
+
+const char* str_json =
+    "{\n"
+    "    \"a\": \"b\",\n"
+    "    \"c\": \"d\",\n"
+    "    \"e\": {\n"
+    "        \"f\": \"g\",\n"
+    "        \"h\": \"i\",\n"
+    "        \"j\": [\n"
+    "            \"k\",\n"
+    "            \"l\"\n"
+    "        ]\n"
+    "    }\n"
+    "}\n"
+    ;
+
+BOOST_AUTO_TEST_CASE(case1)
 {
-    using namespace yaml_parser;
-    using namespace boost::property_tree;
+    std::stringstream ss_yaml(str_yaml);
+    std::stringstream ss_json;
 
-    {
-        ptree pt;
-        read_yaml("test.yaml", pt);
-        write_json("test.json", pt);
-    }
+    ptree pt;
+    read_yaml(ss_yaml, pt);
+    write_json(ss_json, pt);
 
-    {
-        ptree pt;
-        read_json("test2.json", pt);
-        write_yaml("test2.yaml", pt);
-    }
+    BOOST_CHECK_EQUAL(ss_json.str(), str_json);
+}
 
-    {
-        ptree pt;
-        read_xml("test3.xml", pt);
-        write_yaml("test3.yaml", pt);
-    }
+BOOST_AUTO_TEST_CASE(case2)
+{
+    std::stringstream ss_json(str_json);
+    std::stringstream ss_yaml;
 
-    return 0;
+    ptree pt;
+    read_json(ss_json, pt);
+    write_yaml(ss_yaml, pt);
+
+    BOOST_CHECK_EQUAL(ss_yaml.str(), str_yaml);
+}
+
+BOOST_AUTO_TEST_CASE(case3)
+{
+    std::stringstream ss_json1(str_json);
+    std::stringstream ss_json2;
+
+    ptree pt1;
+    ptree pt2;
+    read_json(ss_json1, pt1);
+    write_yaml("Test.yaml", pt1);
+    read_yaml("Test.yaml", pt2);
+    write_json(ss_json2, pt2);
+
+    BOOST_CHECK_EQUAL(ss_json2.str(), str_json);
 }
